abecbb79e190d7d009fd9a369e4f9e3e8df4b35b
Peter Kosov <11111000000@email.com>
2025-09-26 14:01:43 +0800

    chore: bump test-flow version to 0.1.1
    Update the package version from 0.1.0 to 0.1.1 to reflect recent changes or
    preparations for a new release. This helps in version tracking and package
    management.

--- lisp/test-flow.el
+++ lisp/test-flow.el
@@ -1,6 +1,6 @@
 ;;; test-flow.el --- Umbrella for test-flow -*- lexical-binding: t; -*-
 ;; Author: Peter Kosov <11111000000@email.com>
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: tools, lisp, testing
 ;; URL: https://github.com/11111000000/test-flow
